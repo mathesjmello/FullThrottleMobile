da4367d6a39dcf8b5d346eb5c3eebcdbdc9fa78a
Unknown <junqueirademello@gmail.com>
2018-05-16 11:19:02 -0300

    teste cena para mostrar oq é interagivel

---
+++ Assets/Sprits/Interactables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : InteractiveView
+{
+	private bool _highLighted=false;
+	
+	public void ShowHighLights()
+	{
+		if (_highLighted)
+		{
+			gameObject.GetComponent<SpriteRenderer>().color= Color.white;
+			_highLighted = false;
+		}
+		else
+		{
+			gameObject.GetComponent<SpriteRenderer>().color= Color.yellow;
+			_highLighted = true;
+		}
+	}
+}
---
+++ Assets/Sprits/InteractiveView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveView : MonoBehaviour
+{
+
+	public void ShowInteractive()
+	{
+		var listInterectables = FindObjectsOfType<Interactables>();
+		foreach (var interactable in listInterectables)
+		{
+			interactable.ShowHighLights();
+		}
+	}
+}
